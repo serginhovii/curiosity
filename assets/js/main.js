d52006770cebab288ae7a86ac5915040500b042b
unknown <arrazolaser@hotmail.com>
2015-04-09 08:56:55 +0200

    With comments removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,7 +156,7 @@ $(document).ready(function(){
 			$(this).hasClass('selected')?$(this).removeClass('selected'):$(this).addClass('selected');
 			$(this).trigger('cssClassChanged');
 		});
-		/*
+		
 		$(this).bind("cssClassChanged", function(){
 			if(	($(this).hasClass('selected') && !$(this).parent().parent().find(".layerEvent").is(":visible")) ||
 				(!$(this).hasClass('selected') && $(this).parent().parent().find(".layerEvent").is(":visible"))
@@ -181,7 +181,7 @@ $(document).ready(function(){
 				},350
 			);				
 			
-		});*/
+		});
 
 	});
 	
@@ -191,7 +191,7 @@ $(document).ready(function(){
 			$(this).hasClass('selected')?$(this).removeClass('selected'):$(this).addClass('selected');
 			$(this).trigger('cssClassChanged');
 		});
-		/*
+		
 		$(this).bind("cssClassChanged", function(){
 
 			if(	($(this).hasClass('selected') && !$(this).parent().parent().find(".layerAction").is(":visible")) ||
@@ -206,7 +206,7 @@ $(document).ready(function(){
 				},350
 			);
 			
-		});*/
+		});
 
 	});
 	
