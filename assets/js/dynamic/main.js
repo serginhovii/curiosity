cca151d57bac256d109be900d1779729813be7bb
unknown <arrazolaser@hotmail.com>
2015-05-21 20:15:43 +0200

    with autocomplete

--- assets/js/dynamic/main.js
+++ assets/js/dynamic/main.js
@@ -10,6 +10,13 @@ var blurBelow=function(obj){
 	}
 	
 }
+
+function split( val ) {
+  return val.split( /\s+/ );
+}
+function extractLast( term ) {
+  return split( term ).pop();
+}
 $(document).ready(function(){
 	var availableTags = [
         '03 July',
@@ -235,10 +242,37 @@ $(document).ready(function(){
 	});
 
 
-	  $( "input" ).autocomplete({
-	    source: availableTags,
-	    minLength: 3
-	  });
+	      $( "input" )
+      // don't navigate away from the field on tab when selecting an item
+      .bind( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        minLength: 0,
+        source: function( request, response ) {
+          // delegate back to autocomplete, but extract the last term
+          response( $.ui.autocomplete.filter(
+            availableTags, extractLast( request.term ) ) );
+        },
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join( " " );
+          return false;
+        }
+      });
 
 	
 
